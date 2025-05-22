67c6b5366c82a73db04ca5eecf26cd2b1dd2a6f7
Akash2006h <akashkarki352@gmail.com>
2025-05-22 11:45:48 +0530

    Api for users

--- src/app.js
+++ src/app.js
@@ -3,14 +3,46 @@ const app = express()
 const PORT = process.env.PORT || 1234
 const connectDB = require("./config/database.js")
 const User  = require("./models/user.js")
-app.use("/signup",  async (req, res) => {
+app.use(express.json())
+
+
+
+
+app.get("/user", async (req, res) => {
+  const UserEmail = req.body.emailId;
   try{
-    const user = new User({
-      firstName: "Aditya",
-      lastName: "karki",
-      emailId: "aditya@gmail.com",
-      password: "password",
-  })
+    const users = await User.find({emailId: UserEmail})
+    if(users.length == 0){
+          res.status(404).send("No Users");
+    }
+    else{
+          res.send(users)
+    }
+  }
+  catch(err) {
+    console.error("User not found", err.message);    
+  }
+})
+
+app.get("/feed", async(req, res) => {
+  try{
+    const allUsers = await User.find({})
+    res.send(allUsers)
+
+  }
+  catch(err) {
+        res.status(404).send("No Users", err.message);
+
+  }
+})
+
+
+
+
+app.post("/signup",  async (req, res) => {
+  console.log(req.body)
+  try{
+    const user = new User(req.body)
    await user.save(); 
    res.send("user saved succesfully")  
   }
@@ -18,14 +50,6 @@ app.use("/signup",  async (req, res) => {
     console.error("erron on the server", err.message);
   }
 })
-
-
-
-
-
-
-
-
 
 connectDB()
 .then(()=>{
@@ -37,4 +61,10 @@ connectDB()
   .catch((err) => {
     console.error("Database cannot be connected", err.message);
 })
+
+
+
+
+
+
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
 const connectDB = async() => {
+    await mongoose.connect("mongodb+srv://makashmkarki:zWk5nieBKRfYkGA4@tinder.jershmo.mongodb.net/devTinder")
 };
 module.exports = connectDB; 
