4a9b7ede2341f498d572e9b257270f11109d517d
Akash2006h <akashkarki352@gmail.com>
2025-06-17 21:41:59 +0530

    creating an authentication

---
+++ src/utils/validation.js
@@ -0,0 +1,16 @@
+const validator = require("validator");
+const validateSignUp = (req) => {
+  const {firstName, lastName, emailId, password} = req.body;
+  if(!firstName || !lastName){
+    throw new error("name is Not Valid")
+  }
+  else if(!validator.isEmail(emailId)){
+    throw new error("email is Invalid")
+  }
+  else if(!validator.isStrongPassword(password)){
+    throw new error("Please Enter A Strong password")
+  } 
+}
+module.export = {
+  validateSignUp,
+}
